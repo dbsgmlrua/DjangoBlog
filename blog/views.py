f95d66ae6202f580fdb0ceaffdaab41ae86442cf
dbsgmlrua <dbsgmlrua2@gmail.com>
2021-03-10 00:41:57 +0900

    django blog 10강까지

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 def home(request):
@@ -6,6 +7,9 @@ def home(request):
         'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
 
 
 def about(request):
